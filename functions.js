047c764b32d961f77debe843155d2f7f61012758
groovygnome <jrp810@proton.me>
2025-05-23 14:00:23 -0400

    implement reverseString, implement calculator, start implementation of caeserCipher, write tests for each

--- functions.js
+++ functions.js
@@ -1,9 +1,78 @@
 export function capitalize(word) {
+    if (!word) return word;
     let firstLetter = word.at(0).toUpperCase();
     let theRest = word.slice(1);
     return firstLetter.concat(theRest);
 }
 
 export function reverseString(word) {
-    return 'olleh';
+    if (!word) return word;
+    let reversed = word.split('').reverse().join('');
+    return reversed;
+}
+
+export const calculator = {
+    add: function(num1, num2) {
+        if (this.checkNumbers(num1, num2)) {
+            return num1 + num2;
+        } else {
+            return null;
+        }
+    },
+
+    subtract: function(num1, num2) {
+        if (this.checkNumbers(num1, num2)) {
+            return num1 - num2;
+        } else {
+            return null;
+        }
+    },
+
+    divide: function(num1, num2) {
+        if (this.checkNumbers(num1, num2)) {
+            return num1 / num2;
+        } else {
+            return null;
+        }
+    },
+
+    multiply: function(num1, num2) {
+        if (this.checkNumbers(num1, num2)) {
+            return num1 * num2;
+        } else {
+            return null;
+        }
+    },
+
+    checkNumbers: function(num1, num2) {
+        if (!num1 || !num2 || isNaN(num1) || isNaN(num2) || typeof num1 === 'string' || typeof num2 === 'string') return false;
+        return true;
+    }
+};
+
+export function caesarCipher(word, shift) {
+    let charArr = word.split('');
+    newWord = '';
+    charArr.forEach((character) => newWord += shiftCharacter(character, shift));
+    console.log(charArr);
+    return newWord;
+}
+
+function shiftCharacter(character, shift) {
+    // 97 => 122 a => z
+    // 65 => 90 A => Z
+    console.log(character);
+    let upper = false;
+    let lower = false;
+    let code = character.charCodeAt(0);
+    if (code >= 65 && code <= 90) upper = true;
+    if (code >= 97 && code <= 122) lower = true;
+    console.log(lower);
+    console.log(upper);
+    if (!lower && !upper) return character;
+    code += shift;
+    if ((lower && code > 122) || (upper && code > 90)) code -= 26;
+    let newChar = String.fromCharCode(code);
+    console.log(newChar);
+    return newChar;
 }
--- functions.test.js
+++ functions.test.js
@@ -8,6 +8,22 @@ test('capitalze j in john', () => {
     expect(capitalize('john')).toBe('John');
 });
 
+test('no input for capitalize', () => {
+    expect(capitalize('')).toBe('');
+});
+
+test('null input for capitalize', () => {
+    expect(capitalize(null)).toBe(null);
+});
+
+test('undefined input for capitalize', () => {
+    expect(capitalize(undefined)).toBe(undefined);
+});
+
+test('one letter input for capitalize', () => {
+    expect(capitalize('a')).toBe('A');
+});
+
 test('reverse string hello', () => {
     expect(reverseString('hello')).toBe('olleh');
 });
@@ -15,3 +31,43 @@ test('reverse string hello', () => {
 test('reverse string john', () => {
     expect(reverseString('john')).toBe('nhoj');
 });
+
+test('no input for reverseString', () => {
+    expect(reverseString('')).toBe('');
+});
+
+test('null input for reverseString', () => {
+    expect(reverseString(null)).toBe(null);
+});
+
+test('undefined input for reverseString', () => {
+    expect(reverseString(undefined)).toBe(undefined);
+});
+
+test('one letter input for reverseString', () => {
+    expect(reverseString('a')).toBe('a');
+});
+
+test('calculator exists', () => {
+    expect(calculator);
+});
+
+test('calculator addition, subtraction, multiplication, division', () => {
+    expect(calculator.add(2, 2)).toBe(4);
+    expect(calculator.subtract(2, 2)).toBe(0);
+    expect(calculator.divide(2, 2)).toBe(1);
+    expect(calculator.multiply(2, 2)).toBe(4);
+});
+
+test('calculator input is not a number', () => {
+    expect(calculator.add('2', 2)).toBe(null);
+    expect(calculator.subtract('', 2)).toBe(null);
+    expect(calculator.divide(2, 'two')).toBe(null);
+    expect(calculator.multiply(NaN, 2)).toBe(null);
+});
+
+test('caesarCipher abc 3 input', () => {
+    expect(caesarCipher('abc', 3)).toBe('def');
+});
+
+
